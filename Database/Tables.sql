44e7c01ed58f33ad73bde0f6c3a3c88d2e8b055a
BoySanic <boysonicrevived@gmail.com>
2023-10-30 20:58:47 -0700

    Fixed schema syntax errors

--- Database/Tables.sql
+++ Database/Tables.sql
@@ -1,44 +1,51 @@
-create table Services {
-    ServiceID int identity(1,1),
+create table Services (
+    ServiceID int not null auto_increment,
     ServiceName varchar(50),
-    ServiceImportanceID,
+    ServiceImportanceID int,
     ServiceAddress varchar(50),
     ServicePort int,
     ServiceSystemdName varchar(50),
     ServiceLogonName varchar(50),
     ServicePubkey varchar(5000),
     ServiceUptimeCommand varchar(500),
-    ServiceUpString varchar(100)
-};
-create table ServiceHistory {
-    HistoryID int identity(1,1),
+    ServiceUpString varchar(100),
+    PRIMARY KEY (ServiceID)
+);
+create table ServiceHistory (
+    HistoryID int not null auto_increment,
     ServiceID int,
     HistoryDateTime datetime,
     HistoryCommand varchar(500),
     HistoryResult varchar(5000),
-    HistoryPass boolean
-};
-create table ContactMethods{
-    ContactMethodID int identity(1,1),
-    ContactMethodName varchar(50)
-};
-create table Operator {
-    OperatorID int identity(1,1),
-    OperatorName varchar(50)
-};
-create table OperatorMethods{
-    OperatorMethodID int identity(1,1),
+    HistoryPass boolean,
+    Primary Key (HistoryID)
+);
+create table ContactMethods(
+    ContactMethodID int not null auto_increment,
+    ContactMethodName varchar(50),
+    Primary KEY (ContactMethodID)
+);
+create table Operator (
+    OperatorID int not null auto_increment,
+    OperatorName varchar(50),
+    PRIMARY KEY (OperatorID)
+);
+create table OperatorMethods(
+    OperatorMethodID int not null auto_increment,
     OperatorID int,
     ContactMethodID int,
-    OperatorMethodValue varchar(100)
-};
-create table ServiceImportance{
-    ServiceImportanceID int identity(1,1),
-    ServiceImportanceName varchar(50)
-};
-create table ImportanceSeverityTable{
-    ImportanceSeverityID int identity(1,1),
+    OperatorMethodValue varchar(100),
+    PRIMARY KEY (OperatorMethodID)
+);
+create table ServiceImportance(
+    ServiceImportanceID int not null auto_increment,
+    ServiceImportanceName varchar(50),
+    PRIMARY KEY (ServiceImportanceID)
+);
+create table ImportanceSeverityTable(
+    ImportanceSeverityID int not null auto_increment,
     SeverityImportanceID int,
     ImportanceSeverityDowntimeHrs float,
-    ContactMethodID int
-};
+    ContactMethodID int,
+    PRIMARY KEY (ImportanceSeverityID)
+);
